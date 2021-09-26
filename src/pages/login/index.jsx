586901e3049e0035bc3df4c196b006025fcb8982
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-09-26 17:29:34 -0300

    fix: route user verification

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,19 +1,10 @@
 import { UseAuth } from "contexts/auth/hook";
 import { Container } from "./styles";
-import { useHistory } from "react-router-dom";
-import { useEffect } from "react";
 import { Illustration } from "./components/illustration";
 import { Main } from "./components/main";
 
 export function Login() {
-  const { logInWithGoogle, user } = UseAuth();
-  const history = useHistory();
-
-  useEffect(() => {
-    if (user) {
-      history.push("/home");
-    }
-  }, [user, history]);
+  const { logInWithGoogle } = UseAuth();
 
   const handleLogIn = async () => {
     await logInWithGoogle();
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,23 @@
 import { Route, Switch } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import * as Page from "pages";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
+import { useEffect } from "react";
+import { UseAuth } from "contexts/auth/hook";
 
 export function Routes() {
   const location = useLocation();
+  const history = useHistory();
+  const { user } = UseAuth();
+
+  useEffect(() => {
+    if (user) {
+      history.push("/home");
+    } else {
+      history.push("/");
+    }
+  }, [user, history]);
+
   return (
     <AnimatePresence exitBeforeEnter>
       <Switch location={location} key={location.key}>
