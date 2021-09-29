90566ac513983e6618dc9fec53b74efad82d3f78
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-09-29 17:56:04 -0300

    fix: db

--- src/styles/themes/index.js
+++ src/styles/themes/index.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "../../components/themeProvider/ThemeProvider";
+import { ThemeProvider } from "components/themeProvider/ThemeProvider";
 import { darkTheme } from "./colors/dark";
 
 export { darkTheme, ThemeProvider };
