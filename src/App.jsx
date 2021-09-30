157f4227b89be49d6d943df67c3ddb9ed39110f9
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-09-30 12:32:29 -0300

    fix18

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes } from "./routes/routes";
 import { AppContextProvider } from "./contexts/appProvider";
-import { GlobalStyles } from "styles/global";
+import { GlobalStyles } from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider, darkTheme } from "styles/themes";
-import { ToastContainer } from "components/toast";
-import "services/firebase";
+import { ToastContainer } from "./components/toast";
+import "./services/firebase";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
